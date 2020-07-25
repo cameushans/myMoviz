16ce79f84d5a045b6685f2389930ae7b8457dcad
samirdev92 <dev.samir92@gmail.com>
2020-07-25 22:15:10 +0200

    part1 finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
 import './App.css';
-import {Nav,Button,Container,Row} from "reactstrap";
+import {Nav,NavItem,NavLink,Button,Container,Row} from "reactstrap";
 
 import Film from "./component/film";
 
 
+
 function App() {
+
+  var moviesData = [
+    {name:"Star Wars : L'ascension de Skywalker", desc:"La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette ...", img:"/starwars.jpg", note:6.7, vote:5},
+    {name:"Maléfique : Le pouvoir du mal", desc: "Plusieurs années après avoir découvert pourquoi la plus célèbre méchante Disney avait un cœur ...", img:"/maleficent.jpg", note:8.2, vote:3},
+    {name:"Jumanji: The Next Level", desc: "L’équipe est de retour, mais le jeu a changé. Alors qu’ils retournent dans Jumanji pour secourir ...", img:"/jumanji.jpg", note:4, vote:5},
+    {name:"Once Upon a Time... in Hollywood", desc: "En 1969, Rick Dalton – star déclinante d'une série télévisée de western – et Cliff Booth ...", img:"/once_upon.jpg", note:6, vote:7},
+    {name:"La Reine des neiges 2", desc: "Elsa, Anna, Kristoff, Olaf et Sven voyagent bien au-delà des portes d’Arendelle à la recherche de réponses ...", img:"/frozen.jpg", note:4.6, vote:3},
+    {name:"Terminator: Dark Fate", desc: "De nos jours à Mexico. Dani Ramos, 21 ans, travaille sur une chaîne de montage dans une usine automobile...", img:"/terminator.jpg", note:6.1, vote:1},
+  ]
+
+  var moviz = moviesData.map((el, i)=>{
+    return <Film  movieName= {el.name} movieDesc={el.desc} movieImg={el.img} movieNote={el.note} movieVote={el.vote}/>
+    
+  })
+  console.log(moviz)
   return (
 
      <div style={{backgroundColor:"#343B40"}}>
        <Container> 
 
        <Row>
+          <Nav>
+            <span className="navbar-brand">
+              <img src="./logo.png" width="30" height="30" className="d-inline-block align-top" alt="logo" />
+            </span>
+            <NavItem>
+              <NavLink style={{color:'white'}}>Last Releases</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink><Button type="button">11 films</Button></NavLink>
+            </NavItem>
+          </Nav>
             <Nav>
-                <img  src="/logo.png" alt=""/>
+                <img src="/logo.png" alt=""/>
                 <p style={{color:"white"}}>Last Releases</p>
                 <Button color="secondary">11 films</Button>
             </Nav>
+            <Row>
+               {moviz}
+            </Row>
           
-          <Film/>
-    </Row>
+         
+        </Row>
 
    </Container>
       
--- src/component/film.js
+++ src/component/film.js
@@ -3,10 +3,11 @@ import {
     Card, CardImg,
      CardText, CardBody,
      ButtonGroup, Button,
-    Col,Row
+    Col
   } from 'reactstrap';
-import {FontAwesomeIcon} from   "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
+
 
 var tenStars = [] 
 
@@ -14,36 +15,41 @@ for (var i=0; i<10; i++) {
     tenStars.push(<FontAwesomeIcon icon={faStar}/>)
 }
 
-
 var film = []
-for(let i= 0;i<20;i++){
- 
-    film.push( 
-    
-    <Col  lg="4" md="6" xs="12">
-        <Card className='mt-5'>
-                <CardImg top  src="/maleficent.jpg" alt="Card image cap" />
-                <CardBody>
-                    
-                    <CardText >Like <FontAwesomeIcon icon={faHeart}/></CardText>
-                    <CardText >Nombre de vues <FontAwesomeIcon icon={faVideo}/></CardText>
-                    <CardText >Mon avis {tenStars}<ButtonGroup size="sm"><Button size="sm">-</Button><Button size="sm">+</Button></ButtonGroup></CardText>
-                    <CardText >Moyenne {tenStars}< FontAwesomeIcon icon={faStar}/></CardText>
-                    <CardText >Star Wars : L'Ascension de Skywalker</CardText>
-                    <CardText >fjhzdlkfhsdkjfhksjdhfkjdshfkjdshfkdshfdskjfhdskjfhsdk</CardText>
-                
-                </CardBody>
-        </Card>
 
-    </Col>
-    )}
+const Film = (props)=>{
 
+    var tenStarsNote = [] 
+    for (var i=0; i<10; i++) {
+        var color = {}
+        if(i<props.movieNote){
+            color = {color: '#f1c40f'}
+        }
+        tenStarsNote.push(<FontAwesomeIcon style={color}icon={faStar}/>)
+    }
 
-const  Film = (props)=>{
+    // console.log(props)
+
     return(
-     <Row>
-             {film}
-     </Row>
+
+          <Col lg="4" md="6" xs="12">
+            <Card className='mt-5'>
+                    <CardImg top src={props.movieImg} alt="Card image cap" />
+                    <CardBody>
+                        
+                        <CardText >Like <FontAwesomeIcon style={{cursor:'pointer'}} icon={faHeart}/></CardText>
+                        <CardText >Nombre de vues <FontAwesomeIcon icon={faVideo}/></CardText>
+                        <CardText >Mon avis {tenStars}<ButtonGroup size="sm"><Button size="sm">-</Button><Button size="sm">+</Button></ButtonGroup></CardText>
+                        <CardText >Moyenne {tenStarsNote}</CardText>
+                        <CardText >{props.movieName}</CardText>
+                        <CardText style={{height:'75px'}}>{props.movieDesc}</CardText>
+                    
+                    </CardBody>
+            </Card>
+    
+        </Col>
+             
+
     )
 }
 export default Film;
